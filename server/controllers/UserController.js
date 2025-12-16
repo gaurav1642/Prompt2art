c260c54c8ded325b466f3babdc338510e7c9d25e
gaurav1642 <gc0605126@gmail.com>
2025-12-16 19:33:07 +0530

    Validate email and check for duplicate users

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,41 +3,58 @@ import transactionModel from "../models/transactionModel.js"
 import razorpay from 'razorpay';
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import stripe from "stripe";
+
+const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+
 
 // API to register user
 const registerUser = async (req, res) => {
-
     try {
         const { name, email, password } = req.body;
 
-        // checking for all data to register user
+        // check for missing fields
         if (!name || !email || !password) {
-            return res.json({ success: false, message: 'Missing Details' })
+            return res.json({ success: false, message: "Missing Details" });
+        }
+
+        // email validation
+        if (!isValidEmail(email)) {
+            return res.json({ success: false, message: "Invalid email address" });
         }
 
         // hashing user password
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const userData = {
             name,
             email,
             password: hashedPassword,
-        }
+        };
 
-        const newUser = new userModel(userData)
-        const user = await newUser.save()
+        const newUser = new userModel(userData);
+        const user = await newUser.save();
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET
+        );
 
-        res.json({ success: true, token, user: { name: user.name } })
+        res.json({
+            success: true,
+            token,
+            user: { name: user.name }
+        });
 
     } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
+        console.log(error);
+        res.json({ success: false, message: error.message });
     }
-}
+};
+
 
 // API to login user
 const loginUser = async (req, res) => {
@@ -199,4 +216,4 @@ const verifyRazorpay = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser, userCredits, paymentRazorpay, verifyRazorpay }
+export { isValidEmail, registerUser, loginUser, userCredits, paymentRazorpay, verifyRazorpay }
